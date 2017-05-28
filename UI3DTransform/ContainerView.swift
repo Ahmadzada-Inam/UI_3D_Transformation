f34f42701beefb4ef6e6f537c162718b3bc7eb13
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2017-05-28 15:47:45 -0700

    Created ContainerView class

---
+++ UI3DTransform/ContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  ContainerView.swift
+//  UI3DTransform
+//
+//  Created by Inam Ahmad-zada on 2017-05-28.
+//  Copyright © 2017 Inam Ahmad-zada. All rights reserved.
+//
+
+import UIKit
+
+class ContainerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
