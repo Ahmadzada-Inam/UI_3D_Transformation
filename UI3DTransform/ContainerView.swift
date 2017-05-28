085cd942328f7aad603a13ad5f6e046a94381d3d
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2017-05-28 16:07:29 -0700

    Created all the required initializers in the containerview class

--- UI3DTransform/ContainerView.swift
+++ UI3DTransform/ContainerView.swift
@@ -8,14 +8,62 @@
 
 import UIKit
 
-class ContainerView: UIView {
+class ContainerView: UIView, UIGestureRecognizerDelegate {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var savedOriginY: CGFloat = 0.0
+    var parentView: SwitcherView
+    var isFullScreen: Bool = false
+    var index = 0
+    var savedTransform: CATransform3D = CATransform3DIdentity
+    var tapRecognizer: UITapGestureRecognizer!
+
+    init(frame: CGRect, parentView: SwitherView){
+        self.parentView = parenView
+        super.init(frame: frame)
+        self.parentView = parentView
+        savedOriginY = frame.origin.y
+        
+        tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleAnimation))
+        tapRecognizer.numberOfTapsRequired = 1
+        addGestureRecognizer(tapRecognizer)
+        tapRecognizer.delegate = self
+        
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 20.0
+        layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+        
+        layer.edgeAntialiasingMask = CAEdgeAntialiasingMask(rawValue:
+            CAEdgeAntialiasingMask.layerLeftEdge.rawValue |
+            CAEdgeAntialiasingMask.layerTopEdge.rawValue |
+            CAEdgeAntialiasingMask.layerRightEdge.rawValue |
+            CAEdgeAntialiasingMask.layerBottomEdge.rawValue)
     }
-    */
-
+    
+    convenience init(frame: CGRect, parentView: SwitcherView, urlString: String){
+        self.init(frame: frame, parentView: parentView)
+        let webSubview = UIWebView(frame: bounds)
+        webSubview.isUserInteractionEnabled = SwitcherView.enableUserInteractionInSwitcher
+        addSubview(webSubview)
+        if let url = URL(string: urlString){
+            webSubview.loadRequest(URLRequest(url: url))
+        }else{
+            fatalError("<!> could not create url with string: `\(urlString)`")
+        }
+    }
+    
+    convenience init(frame: CGRect, parentView: SwitcherView, customView: UIView){
+        self.init(frame: frame, parentView: parentView)
+        customView.isUserInteractionEnabled = SwitcherView.enableUserInteractionInSwitcher
+        addSubview(customView)
+    }
+    
+    convenience init(frame: CGRect, parentView: SwitcherView, imageName: String){
+        self.init(frame: frame, parentView: parentView)
+        self.layer.contents = UIImage(named: imageName)!.cgImage!
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
