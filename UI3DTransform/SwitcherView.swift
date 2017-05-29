a6789fb28dd447e5c0d8c42c284ff54514c801d4
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2017-05-28 20:32:37 -0700

    Created SwitcherView class

---
+++ UI3DTransform/SwitcherView.swift
@@ -0,0 +1,16 @@
+//
+//  SwitcherView.swift
+//  UI3DTransform
+//
+//  Created by Inam Ahmad-zada on 2017-05-28.
+//  Copyright © 2017 Inam Ahmad-zada. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class SwitcherView: UIView, UIScrollViewDelegate {
+
+    
+
+}
