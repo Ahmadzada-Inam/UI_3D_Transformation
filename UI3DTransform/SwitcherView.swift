363f4189cfa2853732af38e9fe3e162714b6dd47
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2017-05-28 20:50:57 -0700

    Created all the required parameters of switcherview class

--- UI3DTransform/SwitcherView.swift
+++ UI3DTransform/SwitcherView.swift
@@ -11,6 +11,38 @@ import MapKit
 
 class SwitcherView: UIView, UIScrollViewDelegate {
 
+    static let enableUserInteractionInSwitcher = true
     
+    let animationDuration: TimeInterval = 0.2
+    let angle: CGFloat = 55.0
+    let scale: CGFloat = 0.8
+    
+    var containerView: [ContainerView] = []
+    var scrollView: UIScrollView!
+    
+    var switcherViewPadding: CGFloat = 0.0
+    let seperatorDivisor: CGFloat = 3.0
+    var translationDivisors: CGFloat {
+        get{
+            if UIDevice.current.userInterfaceIdiom == .pad{
+                return 4.46
+            }else{
+                return 4.13
+            }
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        standardInitialize()
+    }
 
+    required init(coder: NSCoder) {
+        super.init(coder: coder)!
+        standardInitialize()
+    }
+    
+    func standardInitialize(){
+        
+    }
 }
