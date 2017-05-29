c25de2a9a95f1c8275790f9581764fd2403f0fe9
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2017-05-28 23:03:13 -0700

    Finished viewController's methods

--- UI3DTransform/ViewController.swift
+++ UI3DTransform/ViewController.swift
@@ -13,13 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(SwitcherView(frame: UIScreen.main.bounds))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override var shouldAutorotate: Bool {
+        get{return false}
+    }
 
-
+    override var prefersStatusBarHidden: Bool{
+        get{return true}
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+        get{return .portrait}
+    }
 }
 
